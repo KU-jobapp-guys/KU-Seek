5057d692467f6bcf63906f1921f36e0cf184e703
Parima SANGSIRAKOUP <parima.sa@ku.th>
2025-08-27 20:28:03 +0700

    implement Footer component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import NavBar from './components/layouts/NavBar.vue';
+import Footer from './components/layouts/Footer.vue';
 
 </script>
 
 <template>
   <NavBar role="student"/>
   
-  <div class="min-h-screen w-full">
+  <div class="min-h-screen">
     <RouterView/>
   </div>
+  
+  <Footer />
 
 </template>

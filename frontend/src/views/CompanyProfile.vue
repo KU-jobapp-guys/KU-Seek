8420fbec446e730cf23669a06e4d82fc019fa147
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2025-08-28 00:34:10 +0700

    chore: add mockup data for company profile page

--- frontend/src/views/CompanyProfile.vue
+++ frontend/src/views/CompanyProfile.vue
@@ -1,3 +1,20 @@
+<script setup>
+    const companyData = ref({
+        name: "Urban Mobility Tech",
+        type: "Tech House",
+        website: "[LINE Company (Thailand)](https://linecorp.com/en/)",
+        industry: "Information & Communication Technology",
+        workFields: ["Information and Internet", "Technology"],
+        location: "Bangkok, Thailand",
+        fullLocation: "127 Ratchadamri Road, Gaysorn Tower,  Bangkok 10330 Thailand",
+        size: "101 - 1,000",
+        contacts: "",
+        about: "We are a tech company that focuses on mobility. Our solution is an  on-demand shared service called MuvMi (muvmi.co) to address typical  issues of price and accessibility of microtransit,while Tuk Tuk Hop (www.tuktukhop.com) helps tourists hop around Bangkok main  attractions. We have served over 1.6Mn trips with 100+ EVs operating  everyday. We are expanding our operation and coverage areas rapidly, and will soon expand to other cities(including abroad). We are looking for champions to help us grow.Our team consists of hands-on entrepreneurs with various technical  backgrounds such as receiving PhD from Stanford University, working at  Rambus in Silicon Valley, at Lotus Cars Ltd. inEngland, researching with Volkswagen Electronic Research Laboratory in Silicon  Valley, and over 10 years of experience in designing & manufacturing electric vehicles. On the business side, our team consists of business  background from working at Boston ConsultingGroup and running own businesses.",
+        bannerPhoto: "../assets/images/companies/mockUp/image.png",
+        profilePhoto: "../assets/images/companies/mockUp/logo.png"
+    })
+</script>
+
 <template>
     <h1 class="p-12 font-bold text-6xl">Proflie Page</h1>
 </template>

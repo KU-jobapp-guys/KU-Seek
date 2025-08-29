bd58b8483e4d275b3967794f5ce2b5816d9551af
patth <pattharamon.d@gmail.com>
2025-08-29 08:22:33 +0700

    Make Staff Dashboard UI

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LandingView from '@/views/LandingView.vue'
 import StudentDashboardView from '@/views/StudentDashboardView.vue'
 import CompanyDashboardView from '@/views/CompanyDashboardView.vue'
+import StaffDashboardView from '@/views/StaffDashboardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/company/dashboard',
       name: 'CompanyDashboard',
       component: CompanyDashboardView
+    },
+    {
+      path: '/staff/dashboard',
+      name: 'StaffDashboard',
+      component: StaffDashboardView
     },
   ],
 })

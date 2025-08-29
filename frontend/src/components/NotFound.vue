131c963ed6a6af6fd151068ec75e2617fe1a2838
Parima SANGSIRAKOUP <parima.sa@ku.th>
2025-08-30 00:52:49 +0700

    add customize error404 view

---
+++ frontend/src/components/NotFound.vue
@@ -0,0 +1,14 @@
+<script setup>
+import error404 from '@/assets/icons/error404.svg'
+</script>
+<template>
+  <div class="w-full h-[90vh] flex items-center justify-center">
+    <div class="flex flex-col gap-y-4 items-center justify-center text-center py-8 px-12">
+      <p class="text-2xl font-bold text-blue-900">ERROR</p>
+      <img :src="error404" class="h-[20vh]" />
+      <p class="text-2xl font-bold text-blue-900">This page is outside of the universe</p>
+      <p class="mt-4 text-blue-800">The page you are trying to access doesn't exist or has been moved.<br />Try going back to our homepage</p>
+      <a href="/" class="bg-blue-600 text-white px-4 py-2" >Go to homepage</a>
+    </div>
+  </div>
+</template>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -4,6 +4,7 @@ import JobBoardView from '@/views/JobBoardView.vue'
 import CompanyBoardView from '@/views/CompanyBoardView.vue'
 import StudentDashboardView from '@/views/StudentDashboardView.vue'
 import CompanyDashboardView from '@/views/CompanyDashboardView.vue'
+import NotFound from '@/components/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +33,10 @@ const router = createRouter({
       path: '/company/dashboard',
       name: 'company dashboard',
       component: CompanyDashboardView,
+    },
+    {
+      path: "/:notFound",
+      component: NotFound,
     },
   ],
 })

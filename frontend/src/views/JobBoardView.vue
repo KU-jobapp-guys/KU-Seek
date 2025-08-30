383a8f45d65fbe4a883812736abbdc535a57babf
Parima SANGSIRAKOUP <parima.sa@ku.th>
2025-08-30 09:48:23 +0700

    create overall job board layout

--- frontend/src/views/JobBoardView.vue
+++ frontend/src/views/JobBoardView.vue
@@ -4,4 +4,33 @@ import Header from '@/components/layouts/Header.vue'
 
 <template>
   <Header page="jobBoard" />
+
+  <div class="relative -mt-24 px-[12vw]">
+    <div class="bg-[#F9F9F9] h-[240px] w-full shadow-xl">
+      <div class="w-full h-4 bg-gradient-to-r from-[#FFA100] to-[#FF6A0D]"></div>
+    </div>
+  </div>
+
+  <div class="px-[12vw] p-2 mt-12 flex h-[600px] gap-x-4">
+    <div class="w-full pr-4 h-[600px] gap-y-4 overflow-y-auto">
+      <div v-for="value in [1,2,3,4,5,6]">
+        <div class="h-full bg-[#F9F9F9] px-12 py-8 w-full mb-4 rounded-md shadow-md">
+          <div class="flex justify-between items-center mb-4">
+            <div>
+              <p class="text-xl font-bold">Software Developer #{{ value }}</p>
+              <p>Urban Mobility Tech</p>
+            </div>
+            <div class="bg-gray-300 w-20 h-20 rounded-full" />
+          </div>
+          <p>Bangkok (Remote)</p>
+          <p>- เขียนและพัฒนาโปรแกรมเพื่อให้ทำงานได้ตรงตาม Requirement</p>
+          <p>- เขียนและพัฒนาโปรแกรมเพื่อให้ทำงานได้ตรงตาม Requirement</p>
+        </div>
+      </div>   
+    </div>
+    <div class="w-full bg-[#F9F9F9] p-2 rounded-md shadow-md hidden md:block">
+
+    </div>
+  </div>
+
 </template>

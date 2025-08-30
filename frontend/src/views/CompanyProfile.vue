0e4e380637e4bc5839471a124ffa013d8a8b25a4
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2025-08-30 13:34:16 +0700

    feat: company overview tab is finish

--- frontend/src/views/CompanyProfile.vue
+++ frontend/src/views/CompanyProfile.vue
@@ -4,7 +4,7 @@
     const companyData = ref({
         name: "Urban Mobility Tech",
         type: "Tech House",
-        website: "[LINE Company (Thailand)](https://linecorp.com/en/)",
+        website: {name:" LINE Company (Thailand)", url:" https://linecorp.com/en/"},
         industry: "Information & Communication Technology",
         workFields: ["Information and Internet", "Technology"],
         location: "Bangkok, Thailand",
@@ -65,7 +65,7 @@
                 <img :src="companyData.bannerPhoto" class="rounded-t-[20px] w-[100%]" alt="Company Banner"/>
             </div>
 
-            <div class="relative bg-gradient-to-b from-[#045c3a44] to-white p-8">
+            <div class="relative bg-gradient-to-b from-[#045c3a44] ring-1 ring-[#B1B1B1] ring-inset to-white p-8">
                 <img :src="companyData.profilePhoto" class="absolute -top-20 ring-8 max-w-44 ring-[#BCBCBC] ring-offset-0 rounded-full shadow-[0_4px_6px_rgba(0,0,0,0.25)]" alt="Company Banner"/>
                 
                 
@@ -73,7 +73,7 @@
                     width="46" height="46" viewBox="0 0 24 24" fill="none" alt="edit btn"
                     stroke="#000000" stroke-width="1" stroke-linecap="round"
                     stroke-linejoin="round" class="absolute right-20 lucide lucide-square-pen-icon lucide-square-pen">
-                    <path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2-2v-7"/>
+                    <path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
                     <path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z"/>
                 </svg>
 
@@ -93,7 +93,7 @@
         </section>
 
         <section class="data mt-8">
-            <div class="bg-gradient-to-b from-[#045c3a44] to-white rounded-[20px] w-[100%]  p-8">
+            <div class="bg-gradient-to-b from-[#045c3a44] to-white rounded-[20px] ring-1 ring-[#B1B1B1] ring-inset w-[100%]  p-8">
 
                 <!-- Switch Tab Button IS HEREEEEE -->
                 <div class="flex">
@@ -128,7 +128,38 @@
 
                     <!-- Overview Tab Content -->
                     <div v-if="activeTab === 'overview'" class="space-y-6">
-                        IT OVERRRR
+                        <div class="bg-white ring-1 ring-[#B1B1B1] ring-inset py-12 px-6 rounded-[20px] shadow-[0_4px_6px_rgba(0,0,0,0.25)]">
+                            <div class="flex gap-x-2">
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                    width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                    class="lucide lucide-building2-icon lucide-building-2">
+                                    <path d="M6 22V4a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v18Z"/>
+                                    <path d="M6 12H4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h2"/>
+                                    <path d="M18 9h2a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2h-2"/>
+                                    <path d="M10 6h4"/><path d="M10 10h4"/><path d="M10 14h4"/>
+                                    <path d="M10 18h4"/>
+                                </svg>
+
+                                <h2>Company Overview</h2>
+                            </div>
+
+                            <div class="p-4">
+                                <p><span class="font-medium">Website:</span> <a :href="companyData.website.url" class="underline">{{ companyData.website.name }}</a></p>
+                                <p><span class="font-medium">Industry:</span> {{ companyData.industry }}</p>
+                                <p>
+                                    <span class="font-medium">Specialities: </span>
+                                    
+                                    <span v-for="(item, index) in companyData.workFields" :key="item">
+                                        {{ item }}<span v-if="index < companyData.workFields.length - 1">, </span>
+                                    </span>
+                                </p>
+                                <p><span class="font-medium">Company Size:</span> {{ companyData.size }}</p>
+                                <p><span class="font-medium">Primary Location:</span> {{ companyData.fullLocation }}</p>
+                                <br/>
+                                <p>{{ companyData.about }}</p>
+                            </div>
+                        </div>
                     </div>
 
                     <!-- Jobs Tab Content -->

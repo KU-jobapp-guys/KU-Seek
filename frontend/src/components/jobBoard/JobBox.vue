5a9953c06763c2670bb277aad49cc3cf379ffb25
Parima SANGSIRAKOUP <parima.sa@ku.th>
2025-09-01 22:42:39 +0700

    create JobBox component

---
+++ frontend/src/components/jobBoard/JobBox.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const job = defineProps()
+</script>
+
+<template>
+  <div class="h-full bg-[#F9F9F9] px-12 py-8 w-full mb-4 rounded-md shadow-md">
+    <div class="flex justify-between items-center mb-4">
+      <div>
+        <p class="text-xl font-bold">Software Developer</p>
+        <p>Urban Mobility Tech</p>
+      </div>
+      <div class="bg-gray-300 w-20 h-20 rounded-full" />
+    </div>
+    <p>Bangkok (Remote)</p>
+    <div>
+    <p>- เขียนและพัฒนาโปรแกรมเพื่อให้ทำงานได้ตรงตาม Requirement</p>
+    <p>- เขียนและพัฒนาโปรแกรมเพื่อให้ทำงานได้ตรงตาม Requirement</p>
+    </div>
+  </div>
+</template>

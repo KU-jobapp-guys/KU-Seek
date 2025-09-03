0e235aa5c3747b972ff6bac14c3bf49fdbb4ee6b
Parima SANGSIRAKOUP <parima.sa@ku.th>
2025-09-03 15:41:49 +0700

    fix linting error by changing components name

--- frontend/src/components/layouts/NavBar.vue
+++ frontend/src/components/layouts/NavBar.vue
@@ -16,7 +16,7 @@ const pageList = computed(() => {
   return []
 })
 
-const makeLink = (page: String) => {
+const makeLink = (page: string) => {
   return props.role ? `/${props.role}/${page.toLowerCase().replace(/\s+/g, '-')}` : ''
 }
 </script>
--- frontend/src/views/CompanyDashboardView.vue
+++ frontend/src/views/CompanyDashboardView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import Header from '@/components/layouts/Header.vue'
+import Header from '@/components/layouts/AppHeader.vue'
 </script>
 
 <template>

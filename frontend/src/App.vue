fb3e8c0cfd9c82f3f09b1e866d8b49e4531fa9b2
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2025-09-13 17:32:20 +0700

    feat: add fetch CSRFToken to the App.vue and add it to global when fetch

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,9 +2,28 @@
 import { RouterView } from 'vue-router'
 import NavBar from './components/layouts/NavBar.vue'
 import Footer from './components/layouts/AppFooter.vue'
-import { ref } from 'vue'
+import { ref, provide } from 'vue'
 
 const userRole = ref('student')
+
+const csrfToken = ref<string | null>(null)
+
+const fetchCSRFToken = async () => {
+  try {
+    const response = await fetch('http://localhost:8000/api/csrf-token')
+    const data = await response.json()
+    csrfToken.value = data.token
+  } catch (error) {
+    console.error('Failed to fetch CSRF token:', error)
+  }
+}
+
+
+provide('csrfToken', csrfToken)
+provide('fetchCSRFToken', fetchCSRFToken)
+
+
+fetchCSRFToken()
 </script>
 
 <template>
@@ -16,4 +35,3 @@ const userRole = ref('student')
 
   <Footer />
 </template>
-

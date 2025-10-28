233c3172f6f22a215628f02b24de903b53b24478
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2025-10-29 00:15:25 +0700

    add images, reformatted some divs

--- frontend/src/views/LandingView.vue
+++ frontend/src/views/LandingView.vue
@@ -3,15 +3,15 @@
     <div class="flex flex-col md:flex-row relative z-10">
       <!-- Title Section -->
       <div class="bg-black text-white p-8 md:p-16 flex-1 pb-40">
-        <div class="text-4xl font-bold w-full">KU Seek</div>
-        <div class="mt-4 text-lg w-full">
+        <div class="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold w-full">KU Seek</div>
+        <div class="mt-4 text-sm sm:text-base md:text-lg w-full">
           Your Gateway to Jobs & Internships in Computer Engineering
         </div>
         <div>
           <RouterLink to="/registration" custom v-slot="{ navigate }">
             <button
               @click="navigate"
-              class="px-4 py-2 mt-8 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600"
+              class="px-4 py-2 mt-8 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 text-sm sm:text-base"
             >
               Join Our Network Now
             </button>
@@ -20,12 +20,8 @@
       </div>
 
       <!-- Logo Section -->
-      <div class="bg-orange-400 flex-1 flex justify-center items-center relative p-8 pb-40">
-        <div
-          class="w-72 h-72 bg-gray-200 rounded-md shadow-lg flex items-center justify-center text-black font-semibold"
-        >
-          Image Placeholder
-        </div>
+      <div class="bg-[#FFAC13] flex-1 flex justify-center items-center relative p-8 pb-12">
+          <img src="@/assets/icons/computerWithMug.svg" alt="Computer with mug" class="w-full h-full max-w-md object-contain" />
       </div>
     </div>
 

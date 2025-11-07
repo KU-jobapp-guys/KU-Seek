bfd12f38c2309132acde4c0a7d6775b5d85decea
Parima SANGSIRAKOUP <parima.sa@ku.th>
2025-11-07 19:47:31 +0700

    add profile services for get and update profile

---
+++ frontend/src/services/profileServices.ts
@@ -0,0 +1,55 @@
+import type { Profile } from "@/types/profileType"
+
+export async function getProfileData(user_id: string): Promise<Profile | null> {
+  try {
+    const res = await fetch(`http://localhost:8000/api/v1/users/${user_id}/profile`, {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        'access_token': localStorage.getItem('user_jwt') || ''
+      },
+    })
+    if (!res.ok) {
+      console.error('Fetching profile failed with status:', res.status)
+      return null
+    }
+    return res.json() as Promise<Profile>
+
+  }
+  catch (error) {
+    console.error('Fetching profile error:', error)
+    return null
+  }
+}
+
+export async function updateProfileData(plainData: Profile): Promise<Profile | null>  {
+  try {
+    const { id, ...data } = plainData
+
+    // Replace all null values with ''
+    const cleanedData = Object.fromEntries(
+        Object.entries(data).map(([key, value]) => [key, value === null ? '' : value])
+    );
+    console.log('Updating profile with data:', cleanedData)
+    const res = await fetch(`http://localhost:8000/api/v1/users/profile`, {
+      method: 'PATCH',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        'access_token': localStorage.getItem('user_jwt') || '',
+        'X-CSRFToken': localStorage.getItem('csrf_token') || '',
+      },
+      body: JSON.stringify(cleanedData)
+    })
+    if (!res.ok) {
+      console.error('Updating profile failed with status:', res.status)
+      return null
+    }
+    return res.json() as Promise<Profile>
+  }
+  catch (error) {
+    console.error('Updating profile error:', error)
+    return null
+  }
+}
